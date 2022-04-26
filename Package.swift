3c981970d6eb28fcc1bbeca415d998fcb4c65f8b
Ivan O <ioparin@pm.me>
2022-04-26 22:53:22 +0400

    fixing binary artifact url /2

--- Package.swift
+++ Package.swift
@@ -19,9 +19,10 @@ let package = Package(
             dependencies: ["BinahAI"],
             path: "Sources"
         ),
+        // ❗️ 1.0.4 was release with typo in xc frameowrk name, should be Binah, not BInah
         .binaryTarget(
             name: "BinahAI",
-            url: "https://github.com/galen-it/binah-ios-package/releases/download/v1.0.4/BinahAI.xcframework.zip",
+            url: "https://github.com/galen-it/binah-ios-package/releases/download/1.0.4/BInahAI.xcframework.zip",
             checksum: "9632285dce92de328ce21b0a44abacdee1bfae69f341539e76742eb50a39dafb"
         )
     ]
