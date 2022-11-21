c40cd8d09164ae8016f2c9e3ea59e7b430bb9d38
iv-ann <98310998+iv-ann@users.noreply.github.com>
2022-11-21 21:11:37 +0400

    fixing link to binary target

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "BinahAI",
-            url: "https://github.com/galen-it/binah-ios-package/releases/download/4.9.1/BinahAI.xcframework.zip",
+            url: "https://github.com/darwell-inc/binah-ios-package/releases/download/4.9.1/BinahAI.xcframework.zip",
             checksum: "7799b0aa834a4f42a9ef7856da0b055117ba26f24b78c3f14ccebb00f885c2ab"
         )
     ]
